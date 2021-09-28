56fbde4070549fbef80c87f70c377311faf6ed45
JacobVaerst23 <816769@mystma.org>
2021-09-28 09:26:36 -0500

    comparison attempt 1

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
 #WRITE YOUR CODE IN THIS FILE
+
+#define greaterThan
+def greaterThan(x, y):
+    if x > y:
+        return True
+
+    else:
+        return False 
+
+#define lessThan
+def lessThan(x, y):
+    if x < y:
+        return True
+
+    else:
+        return False
+
+#define equalTO
+def equalTo(x, y):
+    if x == y:
+        return True
+
+    else:
+        return False
+
+#define greaterOrEqual
+def greaterOrEqual(x, y):
+    if x >= y:
+        return True
+
+    else:
+        return False
+
+#define lessOrEqual
+def lessOrEqual(x,y):
+    if x <= y:
+        return True
+
+    else:
+        return False
+
+
